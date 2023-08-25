62bf26a512c87b451713bca0867c5beaa369b603
BanGee24 <130070598+BanGee24@users.noreply.github.com>
2023-08-25 21:03:50 +0100

    MERN Chat app

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://byte-chat-app.onrender.com",
+    origin: "http://localhost:3000",
   },
 });
 
